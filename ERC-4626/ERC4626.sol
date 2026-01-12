112ca3c5d71fa1a438dbb8d23d04eb692d5d0ba1
mandip-kamaliya <144941685+mandip-kamaliya@users.noreply.github.com>
2026-01-12 09:57:39 +0530

    added transferFrom function

--- ERC-4626/ERC4626.sol
+++ ERC-4626/ERC4626.sol
@@ -11,7 +11,7 @@ abstract contract MyERC20{
     mapping (address=>uint256) public balanceOf;
     mapping (address => mapping(address=>uint256)) public allowance;
 
-    event Transfer(address indexed _to , uint256 _amount);
+    event Transfer(address indexed from , address indexed _to , uint256 _amount);
     event Aproval(address indexed owner,address indexed recepient,uint256 _amount); 
 
     constructor(string memory n,string memory s,uint8 d){
@@ -32,12 +32,21 @@ abstract contract MyERC20{
         return true;
     }
 
-    function _transfer(address _to , uint256 _amount) internal {
+    function transferFrom(address from , address to , uint256 amount) external returns(bool){
+        uint256 allowed = allowance[from][msg.sender];
+        if (allowed != type(uint256).max){
+            allowance[from][msg.sender] = allowed - amount;
+        }
+        _transfer(from , to , amount);
+        return true;
+    }
+
+    function _transfer(address _from ,address _to , uint256 _amount) internal {
          require(_to != address(0),"receiver address is empty");
-         require(_amount <= balanceOf[msg.sender],"not enough balance");
-         balanceOf[msg.sender]-=_amount;
+         require(_amount <= balanceOf[_from],"not enough balance");
+         balanceOf[_from]-=_amount;
          balanceOf[_to] += _amount;
-         emit Transfer(_to , _amount);
+         emit Transfer(_from,_to , _amount);
     }
 
 }
